50ead9286f41ea15817a5f0267642104b3305d42
koksickbob <gggggyu123@gmail.com>
2022-11-19 22:16:04 +0900

    총기 발사 스크립트 일부 수정, 재장전 스크립트 일부 구현(수정 필요)

--- Assets/Script/GunControll.cs
+++ Assets/Script/GunControll.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GunControll : MonoBehaviour
 {
-    public Gun currentGun; //Gun ��ũ��Ʈ ����
+    public Gun currentGun;
     public float currentFireRate;
     public bool currentReload = false;
     public AudioSource audioSource;
@@ -15,14 +15,14 @@ public class GunControll : MonoBehaviour
 
     void Update()
     {
-        GunFireRateformula(); //�ѱ� �߻� ����
+        GunFireRateformula(); //총기 발사 수식
         TryFire();
         TryReload();
     }
     private void GunFireRateformula()
     {
         if (currentFireRate > 0)
-            currentFireRate -= Time.deltaTime; //1�ʴ� 1�� ����
+            currentFireRate -= Time.deltaTime; //초당 1발씩 발사
     }
 
     private void TryFire()
@@ -32,24 +32,136 @@ public class GunControll : MonoBehaviour
             Fire();
         }
     }
-    private void Fire() // ���� �ʿ�
+    private void Fire() // 수정 필요
     {
-        currentFireRate = currentGun.fireRate;
-        Shoot();
+        if (!currentReload)
+        {
+            if (currentGun.currentBulletCount > 0)
+                Shoot();
+            else
+            {
+                CancelFineSight();
+                StartCoroutine(ReloadCoroutine());
+            }
+        }
     }
     
-    private void Shoot() // ���� �ʿ�
+    private void Shoot() // 수정 필요
     {
+        currentGun.currentBulletCount--;
+        currentFireRate = currentGun.fireRate;
         PlaySE(currentGun.fire_Sound);
         currentGun.muzzleFlash.Play();
-        Debug.Log("�Ѿ� �߻���");
+
+        StopAllCoroutines();
+        StartCoroutine(RetroActionCoroutine());
+
+        Debug.Log("Bullet Fired");
     }
 
     private void TryReload()
     {
         if (Input.GetKeyDown(KeyCode.R) && currentReload && currentGun.currentBulletCount < currentGun.reloadBulletCount)
         {
-            // fire, shoot ���� �� �ۼ�
+            CancelFineSight();
+            StartCoroutine(ReloatCoroutine());
+        }
+    }
+
+    IEnumerator ReloadCoroutine()
+    {
+        if(currentGun.carryBulletCount> 0)
+        {
+            currentReload = true;
+            currentGun.anim.SetTrigger("Reload");
+
+            currentGun.carryBulletCount += currentGun.currentBulletCount;
+            currentGun.currentBulletCount = 0;
+
+            yield return new WaitForSeconds(currentGun.reloadTime);
+
+            if (currentGun.carryBulletCount >= currentGun.reloadBulletCount)
+            {
+                currentGun.currentBulletCount = currentGun.reloadBulletCount;
+                currentGun.carryBulletCount -= currentGun.reloadBulletCount;
+            }
+            else
+            {
+                currentGun.currentBulletCount = currentGun.carryBulletCount;
+                currentGun.carryBulletCount = 0;
+            }
+            currentReload = false;
+        }
+        else
+        {
+            Debug.Log("No ammo");
+        }
+    }
+
+    private void TryFineSight()
+    {
+        if(Input.GetButtonDown("Fire2") && !currentReload)
+        {
+            FineSight();
+        }
+    }
+
+    public void CancelFineSight()
+    {
+        if (isFineSightMode)
+            FineSight();
+    }
+    private void FineSight()
+    {
+        isFineSightMode = !isFineSightMode;
+        currentGun.anim.SetBool("FineSightMode", isFineSightMode);
+
+        if (isFineSightMode)
+        {
+            StopAllCoroutines();
+            StartCoroutine(FineSightActivateCoroutine());
+        }
+        else
+        {
+            StopAllCoroutines();
+            StartCoroutine(FineSightDeActivateCoroutine());
+        }
+    }
+
+    IEnumerator FineSightDeActivateCoroutine()
+    {
+        while (currentGun.transform.loacalPosition != originPos)
+        {
+            currentGun.transform.localPosition = Vector3.Lerp(currentGun.transform.localPosition, originPos, originPos, 0.2f);
+            yield return null;
+        }
+    }
+
+    IEnumerator RetroActionCoroutine()
+    {
+        Vector3 recoilBack = new Vector3(currentGun.retroActionForce, originPos.y, originPos.z);
+        Vector3 retroActionRecoilback = new Vector3(currentGun.retroActionFineSightForce, currentGun.fineSightOriginPos.y, currentGun.fineSightOriginPos.z);
+
+        if (!isFineSightMode)
+        {
+            currentGun.transform.localPosition = originPos;
+
+            while(currentGun.transform.localPosition.x <= currentGun.retroActionForce - 0.02f)
+            {
+                currentGun.transform.localPosition = Vector3.Lerp(currentGun.transform.localPosition, recoilBack, 0.4f);
+                yield return null;
+            }
+        }
+        else
+        {
+            currentGun.transform.localPosition = currentGun.fineSightOriginPos;
+
+            while (currentGun.transformlocalPosition.x <= currentGun.retroActionFineSightForce - 0.02f)
+            {
+                currentGun.transform.localPosition = Vector3.Lerp(currentGun.transform.localPosition, retroActionRecoilback, 0.4f);
+                yield return null;
+            }
+
         }
     }
     private void PlaySE(AudioClip _clip)
